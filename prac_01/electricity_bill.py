4f20a9faf814af4d36f6b22629f48c49ea151548
Yucong-cheems <yucong.ni@my.jcu.edu.au>
2023-11-06 18:00:31 +0800

    extension done

--- prac_01/electricity_bill.py
+++ prac_01/electricity_bill.py
@@ -8,3 +8,11 @@ daily_use = float(input("Enter daily use in KWH: "))
 while daily_use <= 0:
     print("input must > 0")
     daily_use = float(input("Enter daily use in KWH: "))
+
+Number_of_billing = int(input("Enter number of billing days:"))
+while Number_of_billing <= 0:
+    print("input must > 0")
+    Number_of_billing = int(input("Enter number of billing days:"))
+
+Bill = (cents/100) * daily_use * Number_of_billing
+print(f"Estimated bill: ${Bill}")
